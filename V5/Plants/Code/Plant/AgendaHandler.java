b8b772eb47689e497cccb286db0a230d638e2e35
Circe Carletti <circe.carletti@u-psud.fr>
2021-04-23 16:20:22 +0200

    23/04/21

---
+++ V5/Plants/Code/Plant/AgendaHandler.java
@@ -0,0 +1,32 @@
+package Plant;
+
+
+import com.calendarfx.view.CalendarView;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.layout.VBox;
+
+public class AgendaHandler implements EventHandler<ActionEvent>{
+
+	private Herbier herbier;
+	private VBox contenu;
+    
+	
+	public AgendaHandler(Herbier H) {
+		herbier = H;
+		contenu = new VBox();
+	}
+	
+
+	@Override
+	public void handle(ActionEvent arg0) {
+		contenu.getChildren().removeAll(contenu.getChildren());
+		contenu.setPrefSize(1000, 900);
+		CalendarView myAgenda = herbier.getAgenda();
+		myAgenda.autosize();
+		contenu.getChildren().add(myAgenda);
+		herbier.setContenu(contenu);
+	}
+
+}
---
+++ V5/Plants/Code/Plant/Ecriture.java
@@ -0,0 +1,46 @@
+package Plant;
+
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+class Ecriture
+{
+
+  private String text = new String();
+  private String name = new String();
+  
+  public Ecriture() {
+	  System.out.println("création écriture");
+	 
+  }
+  
+  
+  
+  public static void ecrire(String path, String text) 
+  {
+      System.out.println("écriture : "+text); 
+      System.out.println(path);
+	  FileWriter writer = null;
+       try{
+    	   System.out.println("trace 1");
+            writer = new FileWriter(path, true);
+            writer.write(text,0,text.length());
+       }catch(IOException ex)
+       {
+           ex.printStackTrace();
+       }finally{
+            if(writer != null)
+            {
+                 
+            	try {
+            		System.out.println("trace2");
+					writer.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+            }
+       }
+  }
+} 
